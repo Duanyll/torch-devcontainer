36ded8fce79a4c3268429a7147c26814e2d3b109
duanyll <duanyll@outlook.com>
2025-08-26 18:06:34 +0800

    add minimized conf without c extension

--- csrc/kernel.cuh
+++
@@ -1,3 +0,0 @@
-#include <cstdint>
-
-void my_add_impl(float* a, float* b, float* res, int64_t n);
--- example/libops.pyi
+++
@@ -1,7 +0,0 @@
-import torch
-
-def my_add(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
-    pass
-
-def my_add_cuda(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
-    pass
